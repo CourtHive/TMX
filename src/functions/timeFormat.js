4cea04b81b3086c95f33b38b9b166bedb73f9907
CourtHive <charles@courthive.com>
2023-06-17 16:39:13 -0400

    feat: :sparkles: some schedule time selector smarts

--- src/functions/timeFormat.js
+++
@@ -1,5 +0,0 @@
-import { utilities } from 'tods-competition-factory';
-
-export function timeFormat(time) {
-  return time ? utilities.dateTime.convertTime(time) : '';
-}
---
+++ src/functions/timeStrings.js
@@ -0,0 +1,24 @@
+import { utilities } from 'tods-competition-factory';
+
+export function timeFormat(time) {
+  return time ? utilities.dateTime.convertTime(time) : '';
+}
+
+export function timeStringToSeconds(timeString) {
+  const a = timeString.split(':');
+  return +a[0] * 60 * 60 + +a[1] * 60 + (+a[2] || 0);
+}
+
+export function zeroPad(number) {
+  return number.toString()[1] ? number : '0' + number;
+}
+export function secondsToTimeString(seconds, nextHour) {
+  let hours = Math.floor(seconds / (60 * 60));
+  let minutes = Math.floor((seconds - hours * (60 * 60)) / 60);
+  if (nextHour) {
+    minutes = 0;
+    hours += 1;
+    if (hours > 23) hours = 0;
+  }
+  return `${zeroPad(hours)}:${zeroPad(minutes)}`;
+}
