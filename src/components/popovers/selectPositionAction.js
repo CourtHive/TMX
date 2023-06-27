1414dbe0a4862365ab5a54a34a9d2d083735e25c
courthive <charles@courthive.com>
2023-06-27 07:51:59 -0400

    feat: :sparkles: positionActions modal

--- src/components/popovers/selectPositionAction.js
+++ src/components/popovers/selectPositionAction.js
@@ -1,3 +1,4 @@
+import { selectParticipant } from 'components/modals/selectParticipant';
 import { mutationRequest } from 'services/mutation/mutationRequest';
 import { tipster } from 'components/popovers/tipster';
 
@@ -13,9 +14,7 @@ const actionLabels = {
 export function selectPositionAction({ event, actions, callback }) {
   const handleClick = (action) => {
     ['WITHDRAW', 'BYE', 'REMOVE'].includes(action.type) && noChoiceAction({ action, callback });
-    action.type === 'ASSIGN' && assignParticipant({ action, callback });
-    action.type === 'ALTERNATE' && assignAlternate({ action, callback });
-    action.type === 'SWAP' && swapPositions({ action, callback });
+    ['ASSIGN', 'ALTERNATE', 'SWAP'].includes(action.type) && assignParticipant({ action, callback });
   };
   const options = actions
     ?.filter(({ type }) => actionLabels[type])
@@ -35,38 +34,16 @@ function noChoiceAction({ action, callback }) {
 }
 
 function assignParticipant({ action, callback }) {
-  const postMutation = (result) => (result.success ? callback() : console.log({ result }));
-  const participantId = action.participantsAvailable[0].participantId;
-  const methods = [
-    {
-      params: { ...action.payload, participantId },
-      method: action.method
-    }
-  ];
-  mutationRequest({ methods, callback: postMutation });
-}
+  const onSelection = (params) => {
+    const postMutation = (result) => (result.success ? callback() : console.log({ result }));
+    const methods = [
+      {
+        params: { ...action.payload, ...params },
+        method: action.method
+      }
+    ];
+    mutationRequest({ methods, callback: postMutation });
+  };
 
-function assignAlternate({ action, callback }) {
-  const postMutation = (result) => (result.success ? callback() : console.log({ result }));
-  const alternateParticipantId = action.availableAlternates[0].participantId;
-  const methods = [
-    {
-      params: { ...action.payload, alternateParticipantId },
-      method: action.method
-    }
-  ];
-  mutationRequest({ methods, callback: postMutation });
-}
-
-function swapPositions({ action, callback }) {
-  const postMutation = (result) => (result.success ? callback() : console.log({ result }));
-  const drawPosition = action.availableAssignments[0].drawPosition;
-  action.payload.drawPositions.push(drawPosition);
-  const methods = [
-    {
-      params: { ...action.payload },
-      method: action.method
-    }
-  ];
-  mutationRequest({ methods, callback: postMutation });
+  selectParticipant({ action, onSelection });
 }
