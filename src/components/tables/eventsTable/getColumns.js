1e49bee3482f2e9a3ef27163bd11b80ce4c48d6c
CourtHive <charles@courthive.com>
2023-06-27 18:28:00 -0400

    style: :lipstick: responsive collapse configuration row selectors

--- src/components/tables/eventsTable/getColumns.js
+++ src/components/tables/eventsTable/getColumns.js
@@ -14,6 +14,7 @@ export function getColumns({ exclude = [], eventId, drawId, actions = [] } = {})
       titleFormatter: 'rowSelection',
       formatter: 'rowSelection',
       headerSort: false,
+      responsive: false,
       hozAlign: LEFT,
       width: 5
     },
--- src/components/tables/venuesTable/createVenuesTable.js
+++ src/components/tables/venuesTable/createVenuesTable.js
@@ -86,6 +86,7 @@ export function createVenuesTable({ table } = {}) {
           titleFormatter: 'rowSelection',
           formatter: 'rowSelection',
           headerSort: false,
+          responsive: false,
           hozAlign: LEFT,
           width: 5
         },
@@ -195,6 +196,7 @@ export function createVenuesTable({ table } = {}) {
       titleFormatter: 'rowSelection',
       formatter: 'rowSelection',
       headerSort: false,
+      responsive: false,
       hozAlign: LEFT,
       width: 5
     },
