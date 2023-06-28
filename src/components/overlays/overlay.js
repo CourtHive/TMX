95d2cf02cac5824296fe72c178641972b69beb82
CourtHive <charles@courthive.com>
2023-06-27 21:48:03 -0400

    feat: :construction: overlay WIP

--- src/components/overlays/overlay.js
+++ src/components/overlays/overlay.js
@@ -2,19 +2,25 @@ import { removeAllChildNodes } from 'services/dom/transformers';
 
 import { NONE, OVERLAY_CONTENT, TMX_OVERLAY } from 'constants/tmxConstants';
 
-export function openOverlay({ content }) {
+export function openOverlay({ content } = {}) {
   const overlayContent = document.getElementById(OVERLAY_CONTENT);
   if (typeof content === 'string') {
     overlayContent.innerHTML = content;
-  } else {
+  } else if (content) {
     overlayContent.appendChild(content);
   }
   const tmxOverlay = document.getElementById(TMX_OVERLAY);
-  tmxOverlay.style.display = '';
+  tmxOverlay.style.display = 'block';
+  const closeButton = tmxOverlay.getElementsByClassName('closebtn')?.[0];
+  if (closeButton) {
+    console.log({ closeButton });
+    closeButton.onclick = () => closeOverlay();
+  }
 }
 
 export function closeOverlay() {
+  const overlayContent = document.getElementById(OVERLAY_CONTENT);
+  removeAllChildNodes(overlayContent);
   const tmxOverlay = document.getElementById(TMX_OVERLAY);
-  removeAllChildNodes(tmxOverlay);
   tmxOverlay.style.display = NONE;
 }
