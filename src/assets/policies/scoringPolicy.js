de92eb537776fd762a0a57dabcaa29c605f5f1c5
CourtHive <charles@courthive.com>
2023-07-05 17:09:25 -0400

    refactor: :recycle: cleanup and simply scoringPolicy

--- src/assets/policies/scoringPolicy.js
+++ src/assets/policies/scoringPolicy.js
@@ -20,7 +20,7 @@ export const POLICY_SCORING = {
       {
         categoryNames: [],
         categoryTypes: [],
-        description: 'Best of 3 tiebreak sets',
+        description: 'Best of 3 sets with 7-point tiebreak at 6-6',
         matchUpFormat: 'SET3-S:6/TB7'
       },
       {
@@ -32,7 +32,7 @@ export const POLICY_SCORING = {
         matchUpFormat: 'SET3-S:6/TB7-F:TB10'
       },
       {
-        description: 'One standard tiebreak set to 6, 7-point tiebreak at 6 games all',
+        description: 'One set set to 6, 7-point tiebreak at 6-6',
         matchUpFormat: 'SET1-S:6/TB7'
       },
       {
@@ -40,47 +40,43 @@ export const POLICY_SCORING = {
         matchUpFormat: 'SET3-S:4/TB7'
       },
       {
-        description: 'Two out of three short sets to 4 with 5-point tiebreak at 3 games all',
+        description: 'Two out of three sets to 4 with 5-point tiebreak at 3-3',
         matchUpFormat: 'SET3-S:4/TB5@3'
       },
       {
-        description: 'One short set to 4, 7-point tiebreak at 4 games all',
+        description: 'One set to 4, 7-point tiebreak at 4-4',
         matchUpFormat: 'SET1-S:4/TB7'
       },
       {
-        description: 'One short set to 4, 5-point tiebreak at 3 games all',
+        description: 'One set to 4, 5-point tiebreak at 3-3',
         matchUpFormat: 'SET1-S:4/TB5@3'
       },
       {
-        description: 'Two short sets to 4, 10-point match tiebreak at one set all',
+        description: 'Two sets to 4, 10-point match tiebreak at one set all',
         matchUpFormat: 'SET3-S:4/TB7-F:TB10'
       },
       {
-        description: 'Two short sets to 4, 7-point match tiebreak at one set all',
+        description: 'Two sets to 4, 7-point match tiebreak at one set all',
         matchUpFormat: 'SET3-S:4/TB7-F:TB7'
       },
       {
-        description: 'One no advantage set to 5, tiebreak to 9 at 4-4',
+        description: 'One set to 5, tiebreak to 9 at 4-4',
         matchUpFormat: 'SET1-S:5/TB9@4'
       },
       {
-        description: 'One set to 6 with deciding game at 5 games all',
+        description: 'One set to 6 with deciding game at 5-5',
         matchUpFormat: 'SET1-S:6NOAD'
       },
       {
-        description: 'One short set to 4, deciding game is played at 3 games all',
+        description: 'One set to 4, deciding game is played at 3-3',
         matchUpFormat: 'SET1-S:4NOAD'
       },
       {
-        description: 'Two short sets to 4 with deciding game at 3-3, 7-point match tiebreak at one set all',
-        matchUpFormat: 'SET3-S:4NOAD-F:TB7'
-      },
-      {
-        description: '8 game pro-set with 7 point tiebreak at 8 games all',
+        description: '8 game pro-set with 7 point tiebreak at 8-8',
         matchUpFormat: 'SET1-S:8/TB7'
       },
       {
-        description: '8 game pro-set with 7 point tiebreak at 7 games all',
+        description: '8 game pro-set with 7 point tiebreak at 7-7',
         matchUpFormat: 'SET1-S:8/TB7@7'
       },
       {
@@ -88,12 +84,12 @@ export const POLICY_SCORING = {
         matchUpFormat: 'SET3-S:TB10'
       },
       {
-        description: 'One 10-point tiebreak game',
-        matchUpFormat: 'SET1-S:TB10'
+        description: 'Timed 20 minute game - game based',
+        matchUpFormat: 'SET1-S:T20G'
       },
       {
-        description: 'Timed 20 minute game - game based',
-        matchUpFormat: 'SET1-S:T20'
+        description: 'Timed 20 minute game - point based',
+        matchUpFormat: 'SET1-S:T20P'
       }
     ]
   }
