ac3b42472c9d3d53825894f0e95b75d576d2494e
CourtHive <charles@courthive.com>
2023-07-09 11:52:45 -0400

    feat: :sparkles: enhanced mock participant generator dialog

--- src/components/modals/mockParticipants.js
+++ src/components/modals/mockParticipants.js
@@ -1,13 +1,24 @@
 import { mutationRequest } from 'services/mutation/mutationRequest';
+import { renderForm } from 'components/renderers/renderForm';
+import { genderConstants } from 'tods-competition-factory';
 import { mocksEngine } from 'tods-competition-factory';
 import { context } from 'services/context';
+
+const { FEMALE, MALE, MIXED } = genderConstants;
 
 import { ADD_PARTICIPANTS } from 'constants/mutationConstants';
 
 export function mockParticipants({ callback } = {}) {
-  const generateParticipants = (sex) => {
+  let inputs;
+
+  const generate = () => {
+    const count = inputs.participantsCount.value;
+    const gender = inputs.gender.value;
+    const sex = gender === MIXED ? undefined : gender;
+
     const { participants } = mocksEngine.generateParticipants({
       category: { ratingType: 'WTN', ratingMin: 14, ratingMax: 19.99 },
+      scaledParticipantsCount: parseInt(count),
       scaleAllParticipants: true,
       sex
     });
@@ -18,14 +29,39 @@ export function mockParticipants({ callback } = {}) {
         params: { participants }
       }
     ];
+
     mutationRequest({ methods, callback });
   };
 
   const buttons = [
     { label: 'Cancel', intent: 'none', close: true },
-    { label: 'Mixed', intent: 'is-warning', close: true, onClick: generateParticipants },
-    { label: 'Female', intent: 'is-primary', close: true, onClick: () => generateParticipants('FEMALE') },
-    { label: 'Male', intent: 'is-info', close: true, onClick: () => generateParticipants('MALE') }
+    { label: 'Generate', intent: 'is-info', close: true, onClick: generate }
   ];
-  context.modal.open({ content: 'Generate mock players', buttons });
+
+  const content = (elem) =>
+    (inputs = renderForm(elem, [
+      {
+        options: [
+          { label: 'Mixed', value: MIXED, close: true },
+          { label: 'Female', value: FEMALE, close: true },
+          { label: 'Male', value: MALE, close: true }
+        ],
+        label: 'Participant gender',
+        field: 'gender',
+        value: 32
+      },
+      {
+        options: [
+          { label: '32', value: 32, close: true },
+          { label: '64', value: 64, close: true },
+          { label: '128', value: 128, close: true },
+          { label: '256', value: 256, close: true }
+        ],
+        label: 'Participant count',
+        field: 'participantsCount',
+        value: 32
+      }
+    ]));
+
+  context.modal.open({ title: 'Generate mock players', content, buttons });
 }
