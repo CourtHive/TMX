8ecb239db5eec28ea0099f6b96cfad5c56710170
courthive <charles@courthive.com>
2023-07-11 08:25:04 -0400

    refactor: :recycle: functional decomposition; prep for custom matchUpFormats

---
+++ src/components/drawers/addDraw/addDraw.js
@@ -0,0 +1,45 @@
+import { nameValidator } from 'components/validators/nameValidator';
+import { renderButtons } from 'components/renderers/renderButtons';
+import { tournamentEngine } from 'tods-competition-factory';
+import { renderForm } from 'components/renderers/renderForm';
+import { getFormRelationships } from './formRelationships';
+import { tmxToast } from 'services/notifications/tmxToast';
+import { getFormItems } from './getFormItems';
+import { submitParams } from './submitParams';
+import { context } from 'services/context';
+
+import { RIGHT } from 'constants/tmxConstants';
+
+export function addDraw({ eventId, callback }) {
+  const event = tournamentEngine.getEvent({ eventId }).event;
+  if (!event) return;
+
+  const relationships = getFormRelationships();
+  const items = getFormItems({ event });
+
+  let inputs;
+  const content = (elem) => {
+    inputs = renderForm(elem, items, relationships);
+  };
+
+  const isValid = () => nameValidator(5)(inputs.drawName.value);
+
+  const checkParams = () => {
+    if (!isValid()) {
+      tmxToast({ message: 'Missing Draw name', intent: 'is-danger' });
+    } else if (inputs.matchUpFormat?.value === 'CUSTOM') {
+      console.log('custom');
+    } else {
+      submitParams({ event, inputs, callback });
+    }
+  };
+
+  const buttons = [
+    { label: 'Cancel', intent: 'none', close: true },
+    { label: 'Generate', intent: 'is-primary', onClick: checkParams, close: isValid }
+  ];
+  const title = `Configure draw`;
+
+  const footer = (elem, close) => renderButtons(elem, buttons, close);
+  context.drawer.open({ title, content, footer, context: 'tournament', side: RIGHT, width: '300px' });
+}
---
+++ src/components/drawers/addDraw/submitParams.js
@@ -0,0 +1,62 @@
+import { editTieFormat } from 'components/overlays/editTieFormat.js/editTieFormat';
+import { tmxToast } from 'services/notifications/tmxToast';
+import { generateDraw } from './generateDraw';
+import { drawDefinitionConstants, entryStatusConstants, utilities } from 'tods-competition-factory';
+
+import { AUTOMATED } from 'constants/tmxConstants';
+
+const { FEED_IN, LUCKY_DRAW, MAIN, ROUND_ROBIN, ROUND_ROBIN_WITH_PLAYOFF } = drawDefinitionConstants;
+const { DIRECT_ENTRY_STATUSES } = entryStatusConstants;
+
+export function submitParams({ event, inputs, callback }) {
+  const drawType = inputs.drawType.options[inputs.drawType.selectedIndex].getAttribute('value');
+  const automated = inputs.automated.value === AUTOMATED;
+  const tieFormatName = inputs.tieFormatName?.value;
+  const matchUpFormat = inputs.matchUpFormat?.value;
+  const drawName = inputs.drawName.value;
+
+  const drawSizeValue = inputs.drawSize.value;
+  const groupSize = parseInt(inputs.groupSize.value);
+  const drawSizeInteger = utilities.isConvertableInteger(drawSizeValue) && parseInt(drawSizeValue);
+  const drawSize =
+    ([LUCKY_DRAW, FEED_IN].includes(drawType) && drawSizeInteger) || utilities.nextPowerOf2(drawSizeInteger);
+  const drawEntries = event.entries.filter(
+    ({ entryStage, entryStatus }) => entryStage === MAIN && DIRECT_ENTRY_STATUSES.includes(entryStatus)
+  );
+
+  const eventId = event.eventId;
+  const drawOptions = {
+    matchUpFormat,
+    drawEntries,
+    automated,
+    drawType,
+    drawName,
+    drawSize,
+    eventId
+  };
+
+  if ([ROUND_ROBIN, ROUND_ROBIN_WITH_PLAYOFF].includes(drawType)) {
+    drawOptions.structureOptions = { groupSize };
+  }
+
+  if (drawSizeInteger) {
+    if (tieFormatName === 'CUSTOM') {
+      const setTieFormat = (tieFormat) => {
+        if (tieFormat) {
+          drawOptions.tieFormat = tieFormat;
+          generateDraw({ drawOptions, eventId, callback });
+        }
+      };
+      editTieFormat({ title: 'Custom scorecard', onClose: setTieFormat });
+    } else {
+      drawOptions.tieFormatName = tieFormatName;
+      generateDraw({ drawOptions, eventId, callback });
+    }
+  } else {
+    tmxToast({
+      message: 'Invalid draw size',
+      intent: 'is-warning',
+      pauseOnHover: true
+    });
+  }
+}
