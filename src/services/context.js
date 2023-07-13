190cbb71512f71033fdb9ed417492e5f649a2215
CourtHive <charles@courthive.com>
2023-07-13 16:16:59 -0400

    feat: :sparkles: begin to introduce column config persistence during session

--- src/services/context.js
+++ src/services/context.js
@@ -2,51 +2,21 @@ export const context = {
   tables: [],
 
   ee: null, // EventEmitter
-  dfx: null,
-  draws: {},
-  rrDraw: null,
-  treeDraw: null,
-  draw_types: [],
 
-  display: null,
-  displayTab: null, // function to switch tabs programatically
-
-  ccTime: 0, // contextClick time; used to prevent Safari event propagation to click
+  drawer: null,
+  modal: null,
 
   state: {
-    edit: false,
-    admin: false,
     authorized: false,
-    lastCloudSave: false,
-    exportPlayerDB: false,
-    manual_ranking: false,
-    manual_pin: undefined
+    admin: false
   },
 
-  settings: {
-    fetchRegisteredPlayers: false
-  },
-
-  // keep track of displayed components
   displayed: {
-    team: null,
-    euid: null,
-    location: null,
-    draw_event: null,
     schedule_day: null,
-    dual_match: null,
-    players: {
-      not_signed_in: true
-    }
+    draw_event: null
   },
 
-  schedule_filters: {
-    event: '',
-    round: '',
-    court: '',
-    teamid: ''
-  },
-
-  event_config: null,
-  filters: []
+  columns: {
+    'ratings.utr.utrRating': false
+  }
 };
