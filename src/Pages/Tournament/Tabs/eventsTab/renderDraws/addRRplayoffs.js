fb3cb7b3a87542f0b69e9b06e9d0ef785d506178
courthive <charles@courthive.com>
2023-07-14 21:30:14 -0400

    feat: :sparkles: rr playoff generation

--- src/Pages/Tournament/Tabs/eventsTab/renderDraws/addRRplayoffs.js
+++ src/Pages/Tournament/Tabs/eventsTab/renderDraws/addRRplayoffs.js
@@ -1,24 +1,15 @@
 import { mutationRequest } from 'services/mutation/mutationRequest';
 import { renderForm } from 'components/renderers/renderForm';
-import { tournamentEngine } from 'tods-competition-factory';
 import { tmxToast } from 'services/notifications/tmxToast';
-import { addRRplayoffs } from './addRRplayoffs';
 import { isFunction } from 'functions/typeOf';
 import { context } from 'services/context';
 
-import { ADD_PLAYOFF_STRUCTURES } from 'constants/mutationConstants';
 import { NONE, PLAYOFF_NAME_BASE } from 'constants/tmxConstants';
+import { ADD_PLAYOFF_STRUCTURES } from 'constants/mutationConstants';
 
-export function addStructures({ drawId, structureId, callback }) {
-  const result = tournamentEngine.getAvailablePlayoffProfiles({ drawId, structureId });
-  const sum = (p) => p.finishingPositions.reduce((a, b) => (a || 0) + (b || 0));
-
-  if (!result.playoffRoundsRanges && result.finishingPositionsAvailable?.length) {
-    return addRRplayoffs({ ...result, drawId, structureId, callback });
-  }
-
-  const fields = result.playoffRoundsRanges
-    ?.sort((a, b) => sum(a) - sum(b))
+export function addRRplayoffs({ callback, drawId, structureId, playoffFinishingPositionRanges }) {
+  const fields = playoffFinishingPositionRanges
+    // ?.sort((a, b) => sum(a) - sum(b))
     .map(({ finishingPositionRange }) => ({
       label: finishingPositionRange,
       field: finishingPositionRange,
@@ -37,13 +28,6 @@ export function addStructures({ drawId, structureId, callback }) {
     return;
   }
 
-  const modifyPlaceholders = (value) => {
-    fields.forEach(({ label, fieldPair }) => {
-      const elem = document.getElementById(fieldPair.id);
-      if (elem) elem.placeholder = `${value} ${label}`;
-    });
-  };
-
   const nameBase = {
     onChange: (e) => modifyPlaceholders(e.target.value),
     onKeyDown: (e) => e.key === 'Tab' && modifyPlaceholders(e.target.value),
@@ -53,37 +37,51 @@ export function addStructures({ drawId, structureId, callback }) {
     id: 'nameBase'
   };
 
+  const modifyPlaceholders = (value) => {
+    fields.forEach(({ label, fieldPair }) => {
+      const elem = document.getElementById(fieldPair.id);
+      if (elem) elem.placeholder = `${value} ${label}`;
+    });
+  };
+
   const options = [nameBase].concat(fields);
 
   let inputs;
 
   const onClick = () => {
-    const checkedRanges = result.playoffRoundsRanges.filter((range) => inputs[range.finishingPositionRange]?.checked);
-
     const playoffStructureNameBase = inputs.nameBase.value;
-    const playoffAttributes = {};
-    const roundProfiles = checkedRanges.map(({ roundNumber, finishingPositionRange }) => {
-      const name = inputs[`${finishingPositionRange}-name`]?.value;
-      if (name) {
-        playoffAttributes[`0-${roundNumber}`] = { name };
-      }
-      return { [roundNumber]: 1 };
+    const checkedRanges = playoffFinishingPositionRanges.filter(
+      (range) => inputs[range.finishingPositionRange]?.checked
+    );
+    const playoffGroups = checkedRanges.map(({ finishingPosition, finishingPositionRange }) => {
+      const input = inputs[`${finishingPositionRange}-name`];
+      const structureName = input?.value || input?.placeholder || PLAYOFF_NAME_BASE;
+      return {
+        finishingPositions: [finishingPosition],
+        drawType: 'SINGLE_ELIMINATION',
+        structureName
+      };
     });
+
     const methods = [
       {
-        params: { drawId, structureId, playoffAttributes, roundProfiles, playoffStructureNameBase },
+        params: { drawId, structureId, playoffFinishingPositionRanges, playoffGroups, playoffStructureNameBase },
         method: ADD_PLAYOFF_STRUCTURES
       }
     ];
+
     const postMutation = (result) => {
       if (result.success) {
         isFunction(callback) && callback();
       } else {
+        console.log({ result });
         tmxToast({ message: result.error?.message || 'Error', intent: 'is-danger' });
       }
     };
+
     mutationRequest({ methods, callback: postMutation });
   };
+
   const content = (elem) => (inputs = renderForm(elem, options));
 
   context.modal.open({
