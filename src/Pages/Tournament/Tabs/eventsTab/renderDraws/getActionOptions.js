bd9f94782ac9f0685872b333a17f30beee32bad7
courthive <charles@courthive.com>
2023-07-17 10:13:54 -0400

    feat: :sparkles: add dialog to delete multiple flights; refactor to extract modal logic;

--- src/Pages/Tournament/Tabs/eventsTab/renderDraws/getActionOptions.js
+++ src/Pages/Tournament/Tabs/eventsTab/renderDraws/getActionOptions.js
@@ -1,68 +1,13 @@
 import { updateTieFormat } from 'components/overlays/editTieFormat.js/updateTieFormat';
 import { navigateToEvent } from 'components/tables/common/navigateToEvent';
-import { mutationRequest } from 'services/mutation/mutationRequest';
-import { renderForm } from 'components/renderers/renderForm';
+import { deleteFlights } from 'components/modals/deleteFlights';
 import { eventConstants } from 'tods-competition-factory';
 import { editMatchUpFormat } from './editMatchUpFormat';
-import { context } from 'services/context';
-
-import { DELETE_FLIGHT_AND_DRAW } from 'constants/mutationConstants';
-import { NONE } from 'constants/tmxConstants';
-import { wordValidator } from 'components/validators/wordValidator';
 
 const { TEAM } = eventConstants;
 
 export function getActionOptions({ eventData, drawId, structureId, structureName }) {
   const eventId = eventData.eventInfo.eventId;
-
-  const deleteDraw = () => {
-    const drawData = eventData?.drawsData?.find((data) => data.drawId === drawId);
-
-    let inputs;
-    const deleteAction = () => {
-      const auditData = { auditReason: inputs['drawDeletionReason'].value };
-      const methods = [{ method: DELETE_FLIGHT_AND_DRAW, params: { eventId, drawId, auditData } }];
-      const postMutation = (result) => result.success && navigateToEvent({ eventId });
-      mutationRequest({ methods, callback: postMutation });
-    };
-    const items = [
-      {
-        text: `Please provide a reason for draw deletion.`
-      },
-      {
-        placeholder: 'Explanation',
-        field: 'drawDeletionReason',
-        validator: wordValidator(5),
-        error: 'Five word minimum',
-        focus: true
-      },
-      {
-        text: `This action cannot be undone!`
-      }
-    ];
-    const enableSubmit = ({ inputs }) => {
-      const value = inputs['drawDeletionReason'].value;
-      const isValid = wordValidator(5)(value);
-      const deleteButton = document.getElementById('deleteDraw');
-      if (deleteButton) deleteButton.disabled = !isValid;
-    };
-    const relationships = [
-      {
-        control: 'drawDeletionReason',
-        onInput: enableSubmit
-      }
-    ];
-    const content = (elem) => (inputs = renderForm(elem, items, relationships));
-
-    context.modal.open({
-      title: `Delete ${drawData.drawName}`,
-      content,
-      buttons: [
-        { label: 'Cancel', intent: NONE, close: true },
-        { label: 'Delete', id: 'deleteDraw', disabled: true, intent: 'is-danger', close: true, onClick: deleteAction }
-      ]
-    });
-  };
 
   return [
     {
@@ -82,7 +27,7 @@ export function getActionOptions({ eventData, drawId, structureId, structureName
       label: 'View entries'
     },
     {
-      onClick: deleteDraw,
+      onClick: () => deleteFlights({ eventData, drawIds: [drawId] }),
       label: 'Delete draw'
     }
   ];
