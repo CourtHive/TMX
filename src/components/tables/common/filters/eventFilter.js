259135c27eeabcbfe1691640b0ee54b8ed78d165
CourtHive <charles@courthive.com>
2023-09-13 14:26:26 -0400

    fix: :bug: fix participant and matchUp filters

--- src/components/tables/common/filters/eventFilter.js
+++ src/components/tables/common/filters/eventFilter.js
@@ -7,14 +7,14 @@ export function getEventFilter(table) {
 
   const eventFilter = (rowData) => rowData?.eventIds?.includes(filterValue);
   const updateEventFilter = (eventId) => {
-    if (filterValue) table.removeFilter(eventFilter);
+    table.removeFilter(eventFilter);
     filterValue = eventId;
     if (eventId) table.addFilter(eventFilter);
   };
   const events = tournamentEngine.getEvents().events || [];
   const allEvents = {
     label: `<span style='font-weight: bold'>${ALL_EVENTS}</span>`,
-    onClick: updateEventFilter,
+    onClick: () => updateEventFilter(),
     close: true
   };
   const eventOptions = [allEvents, { divider: true }].concat(
