fb444b1d2a311ce48f93b28877c3378b9aad4e13
courthive <charles@courthive.com>
2023-10-02 17:29:05 -0400

    feat: :sparkles: ability to open map of venue location from schedule on matchUp display

--- src/Pages/Tournament/Tabs/eventsTab/getEventHandlers.js
+++ src/Pages/Tournament/Tabs/eventsTab/getEventHandlers.js
@@ -2,6 +2,7 @@ import { tournamentEngine, participantConstants } from 'tods-competition-factory
 import { selectPositionAction } from 'components/popovers/selectPositionAction';
 import { openScorecard } from 'components/overlays/scorecard/scorecard';
 import { enterMatchUpScore } from 'services/transitions/scoreMatchUp';
+import { matchUpActions } from 'components/popovers/matchUpActions';
 import { tipster } from 'components/popovers/tipster';
 
 import { BOTTOM } from 'constants/tmxConstants';
@@ -61,15 +62,47 @@ export function getEventHandlers({ eventData, callback }) {
   return {
     centerInfoClick: () => console.log('centerInfo click'),
     roundHeaderClick: (props) => {
-      console.log('round click', props);
+      console.log('round click');
       if (props?.pointerEvent) {
         tipster({ items: roundActions, target: props.pointerEvent.target, config: { placement: BOTTOM } });
       }
     },
-    scheduleClick: () => console.log('schedule click'),
-    venueClick: () => console.log('venue click'), // when no venueClick fall back to scheduleClick
+    scheduleClick: (props) => {
+      console.log('schedule click');
+      if (props?.pointerEvent) {
+        props.pointerEvent.stopPropagation();
+        matchUpActions({ pointerEvent: props.pointerEvent, matchUp: props.matchUp });
+      }
+    },
+    venueClick: (props) => {
+      console.log('venue click', props); // when no venueClick fall back to scheduleClick
+      if (props?.pointerEvent && props.matchUp?.schedule?.venueId) {
+        const venueId = props.matchUp.schedule.venueId;
+        const address = tournamentEngine.findVenue({ venueId })?.venue?.addresses?.[0];
+        const { latitude, longitude } = address || {};
+        const google = `https://www.google.com/maps/@?api=1&map_action=map&center=${latitude}%2C${longitude}&zoom=18&basemap=satellite`;
+        const bing = `https://bing.com/maps/default.aspx?cp=${latitude}~${longitude}&lvl=17&style=h`;
+        const openMap = (provider) => {
+          const url = provider && latitude && longitude && ((provider === 'google' && google) || bing);
+          window.open(url, '_blank');
+        };
+        const venueOptions = [
+          { heading: 'Venue Location' },
+          { divider: 'divider' },
+          {
+            onClick: () => openMap('google'),
+            text: 'Open in Google Maps'
+          },
+          {
+            onClick: () => openMap('bing'),
+            text: 'Open in Bing Maps'
+          }
+        ];
 
-    matchUpClick: () => {},
+        tipster({ items: venueOptions, target: props.pointerEvent.target, config: { placement: BOTTOM } });
+      }
+    },
+    matchUpClick: (params) => console.log('matchUpClick', params),
     scoreClick: (props) => {
       const { matchUp = {}, side, sideIndex } = props;
       const sideNumber = side?.sideNumber || sideIndex + 1;
