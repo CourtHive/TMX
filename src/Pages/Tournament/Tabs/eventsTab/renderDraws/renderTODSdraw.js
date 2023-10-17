68ab236565199d087acde80c0db0211ca77cc382
CourtHive <charles@courthive.com>
2023-10-17 08:49:39 -0400

    chore: :package: bump factory to 1.7.8

--- src/Pages/Tournament/Tabs/eventsTab/renderDraws/renderTODSdraw.js
+++ src/Pages/Tournament/Tabs/eventsTab/renderDraws/renderTODSdraw.js
@@ -138,6 +138,9 @@ export function renderTODSdraw({ eventId, drawId, structureId, compositionName }
       const drawControlItems = adHocActions;
       const drawControl = document.getElementById(DRAW_CONTROL);
       controlBar({ target: drawControl, items: drawControlItems });
+    } else if (structure.stage === drawDefinitionConstants.VOLUNTARY_CONSOLATION) {
+      console.log('voluntary controlBar with [View participants]');
+      // use modal with eligible players and selected players highlighted
     }
 
     // FILTER: participantFilter used to filter matchUps from all rounds in target structure
