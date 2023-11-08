7c48c36e4add77d419c3f74a0e9aafc77faaf9a9
CourtHive <charles@courthive.com>
2023-11-07 19:20:08 -0500

    feat: :sparkles: extend setDev

--- src/services/setDev.js
+++ src/services/setDev.js
@@ -1,5 +1,8 @@
+import { exportTournamentRecord } from 'components/modals/exportTournamentRecord';
+import { connectSocket, disconnectSocket, emitTmx } from './messaging/socketIo';
 import { addOrUpdateTournament } from 'services/storage/addTournament';
 import { loadTournament } from 'Pages/Tournament/tournamentDisplay';
+import { mutationRequest } from './mutation/mutationRequest';
 import { getLoginState } from './authentication/loginState';
 import * as factory from 'tods-competition-factory';
 import { tmx2db } from 'services/storage/tmx2db';
@@ -9,11 +12,37 @@ import { env } from 'settings/env';
 
 import { TOURNAMENT } from 'constants/tmxConstants';
 
-import { connectSocket, disconnectSocket, emitTmx } from './messaging/socketIo';
-import { exportTournamentRecord } from 'components/modals/exportTournamentRecord';
+function functionOrLog(s, results) {
+  return typeof window.dev[s] === 'function'
+    ? window.dev[s](results)
+    : // eslint-disable-next-line no-console
+      (window.dev.allSubscriptions || window.dev[s]) && console.log(s, results);
+}
+
+const subscriptions = {
+  addDrawDefinition: (results) => functionOrLog('addDrawDefinition', results),
+  deletedDrawIds: (results) => functionOrLog('deletedDrawIds', results),
+  addVenue: (results) => functionOrLog('addVenue', results),
+  deleteVenue: (results) => functionOrLog('deleteVenue', results),
+  modifyMatchUp: (results) => functionOrLog('modifyMatchUp', results),
+  addMatchUps: (results) => functionOrLog('addMatchUps', results),
+  deletedMatchUpIds: (results) => functionOrLog('deletedMatchUpIds', results),
+  publishEvent: (results) => functionOrLog('publishEvent', results),
+  unPublishEvent: (results) => functionOrLog('unPublishEvent', results),
+  publishEventSeeding: (results) => functionOrLog('publishEventSeeding', results),
+  unPublishEventSeeding: (results) => functionOrLog('unPublishEventSeeding', results),
+  modifyDrawEntries: (results) => functionOrLog('modifyDrawEntries', results),
+  modifyEventEntries: (results) => functionOrLog('modifyEventEntries', results),
+  modifyDrawDefinition: (results) => functionOrLog('modifyDrawDefinition', results),
+  modifyParticipants: (results) => functionOrLog('modifyParticipants', results),
+  modifyVenue: (results) => functionOrLog('modifyVenue', results),
+  modifySeedAssignments: (results) => functionOrLog('modifySeedAssignments', results),
+  modifyPositionAssignments: (results) => functionOrLog('modifyPositionAssignments', results)
+};
 
 export function setDev() {
   if (!window.dev) {
+    // eslint-disable-next-line no-console
     console.log('%c dev initialized', 'color: yellow');
     window.dev = {};
   } else {
@@ -21,6 +50,11 @@ export function setDev() {
   }
 
   const help = () => console.log('set window.socketURL for messaging');
+  const modifyTournament = (methods) => {
+    mutationRequest({ methods });
+    // eslint-disable-next-line no-console
+    console.log('%cModified! Refresh or modify history to view changes...', 'color: lightgreen');
+  };
 
   addDev({
     getTournament: () => factory.tournamentEngine.getState()?.tournamentRecord,
@@ -36,6 +70,8 @@ export function setDev() {
   addDev({ connectSocket, disconnectSocket, emitTmx });
   addDev({ tmx2db, load, exportTournamentRecord });
   addDev({ env, tournamentContext: context });
+
+  factory.setSubscriptions({ subscriptions });
 }
 
 function addDev(variable) {
@@ -44,6 +80,7 @@ function addDev(variable) {
   try {
     Object.keys(variable).forEach((key) => (window.dev[key] = variable[key]));
   } catch (err) {
+    // eslint-disable-next-line no-console
     console.log('production environment');
   }
 }
@@ -73,6 +110,7 @@ function modifyTournament(methods) {
 function generateMockTournament(params) {
   const { tournamentRecord, error } = factory.mocksEngine.generateTournamentRecord(params);
   if (error) {
+    // eslint-disable-next-line no-console
     console.log({ error });
   } else {
     addAndDisplay(tournamentRecord);
