1860dfa258cfc21f90bba8dc223d209ecde4ea76
CourtHive <charles@courthive.com>
2024-01-29 07:14:38 -0500

    refactor: :recycle: avoid dev cancelled log if already cancelled

--- src/services/authentication/checkDevState.js
+++ src/services/authentication/checkDevState.js
@@ -1,6 +1,6 @@
 export function checkDevState() {
   const notLocal = !window.location.host.startsWith('localhost');
-  if (notLocal) {
+  if (notLocal && window['dev']) {
     delete window['dev'];
     console.log('%c dev cancelled', 'color: cyan');
   }
