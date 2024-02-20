19aaabf99d26a1830c8f3af297060b789ef66db0
courthive <charles@courthive.com>
2024-02-20 15:56:56 -0500

    refactor: :recycle: cleanup and re-enable functionality after previous refactors

--- src/services/apis/servicesApi.js
+++ src/services/apis/servicesApi.js
@@ -4,10 +4,18 @@ export async function requestTournament({ tournamentId }) {
   return await baseApi.post('/factory/fetch', { tournamentId });
 }
 
+export async function getProvider({ providerId }) {
+  return await baseApi.post('/provider/detail', { providerId });
+}
+
 export async function getCalendar({ providerAbbr }) {
   return await baseApi.post('/provider/calendar', { providerAbbr });
 }
 
 export async function getProviders() {
   return await baseApi.post('/provider/allProviders', {});
+}
+
+export async function sendTournament({ tournamentRecord }) {
+  return await baseApi.post('/factory/save', { tournamentRecord });
 }
