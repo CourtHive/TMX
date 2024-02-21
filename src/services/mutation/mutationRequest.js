2830418f4998d5c240e8270f878873a6a280199c
CourtHive <charles@courthive.com>
2024-02-21 08:00:13 -0500

    refactor: :sparkles: add check for tournament date range before mutation

--- src/services/mutation/mutationRequest.js
+++ src/services/mutation/mutationRequest.js
@@ -17,6 +17,7 @@ export function mutationRequest({ methods, engine = TOURNAMENT_ENGINE, callback 
 
   const getProviderId = (tournamentRecord) => tournamentRecord?.parentOrganisation?.organisationId;
   const tournamentRecords = factoryEngine.getState()?.tournamentRecords ?? {};
+
   const tournamentIds =
     engine === TOURNAMENT_ENGINE
       ? [factoryEngine.getTournamentId()]
@@ -28,40 +29,61 @@ export function mutationRequest({ methods, engine = TOURNAMENT_ENGINE, callback 
         : Object.values(tournamentRecords)?.map(getProviderId),
     )
     .filter(Boolean);
-
   if (providerIds.length > 1) return tmxToast({ message: 'Multiple providers', intent: 'is-danger' });
 
-  const state = getLoginState();
+  const now = new Date().getTime();
+  const inDateRange = Object.values(tournamentRecords).every((record) => {
+    const startTime = new Date(record.startDate).getTime();
+    const endTime = new Date(record.endDate).getTime();
+    return startTime && endTime && startTime >= now && endTime <= now;
+  });
 
-  if (providerIds.length) {
-    // if the tournamentRecord(s) are associated with a providerId, check permissions
-    if (!state) return tmxToast({ message: 'Not logged in', intent: 'is-warning' });
-    const isProvider = !!state?.providerIds?.includes(providerIds[0]);
-    const isSuperAdmin = state?.roles?.includes(SUPER_ADMIN);
-    if (!isProvider && !isSuperAdmin) return tmxToast({ message: 'Not authorised', intent: 'is-danger' });
-    if (!isProvider && isSuperAdmin) {
-      const impersonating = context.provider?.organisationId === providerIds[0];
-      if (!impersonating) {
-        const impersonateProvider = () => {
-          context.provider = { organisationId: providerIds[0] };
-          return makeMutation({ methods, factoryEngine, tournamentIds, completion });
-        };
-        return tmxToast({
-          action: {
-            onClick: impersonateProvider,
-            text: 'Impersonate?',
-          },
-          message: 'Super Admin',
-          intent: 'is-danger',
-        });
-      }
-    }
-  }
-
-  return makeMutation({ methods, factoryEngine, tournamentIds, completion });
+  const mutate = (saveLocal) => makeMutation({ methods, factoryEngine, tournamentIds, completion, saveLocal });
+  if (!inDateRange) return queryDateRange({ providerIds, mutate });
+  if (providerIds.length) return checkPermissions({ providerIds, mutate });
+  return mutate();
 }
 
-function makeMutation({ methods, completion, factoryEngine, tournamentIds }) {
+function queryDateRange({ providerIds, mutate }) {
+  const onClick = () => (providerIds?.length ? checkPermissions({ providerIds, mutate }) : mutate());
+  return tmxToast({
+    action: { onClick, text: 'Modify?' },
+    message: 'Not in date range',
+    intent: 'is-danger',
+  });
+}
+
+function checkPermissions({ providerIds, mutate }) {
+  const state = getLoginState();
+  // if the tournamentRecord(s) are associated with a providerId, check permissions
+  if (!state) return tmxToast({ message: 'Not logged in', intent: 'is-warning' });
+
+  const isProvider = !!(
+    state?.providerIds?.includes(providerIds[0]) || state?.provider?.organisationId === providerIds[0]
+  );
+  const isSuperAdmin = state?.roles?.includes(SUPER_ADMIN);
+  const impersonating = context.provider?.organisationId === providerIds[0];
+
+  if (!isProvider && !isSuperAdmin) return tmxToast({ message: 'Not authorized', intent: 'is-danger' });
+  if (!isProvider && isSuperAdmin && !impersonating) {
+    const impersonateProvider = () => {
+      context.provider = { organisationId: providerIds[0] };
+      return mutate(false);
+    };
+    return tmxToast({
+      action: {
+        onClick: impersonateProvider,
+        text: 'Impersonate?',
+      },
+      message: 'Super Admin',
+      intent: 'is-danger',
+    });
+  }
+
+  return mutate(!isProvider && !(isSuperAdmin && impersonating));
+}
+
+function makeMutation({ methods, completion, factoryEngine, tournamentIds, saveLocal = true }) {
   if (window['dev']?.params) {
     for (const method of methods) {
       if (window['dev'].params[method.method]) {
@@ -78,7 +100,7 @@ function makeMutation({ methods, completion, factoryEngine, tournamentIds }) {
 
   if (result?.success) {
     emitTmx({ data: { type: 'executionQueue', payload: { methods, tournamentIds } } });
-    saveTournamentRecord();
+    if (saveLocal) saveTournamentRecord();
   }
 
   return completion(result);
