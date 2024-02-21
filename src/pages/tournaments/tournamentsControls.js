09ea697b42982599acb93c57a4b3dcad69c94e06
CourtHive <charles@courthive.com>
2024-02-21 05:50:02 -0500

    refactor: :fire: remove outdated code

--- src/pages/tournaments/tournamentsControls.js
+++ src/pages/tournaments/tournamentsControls.js
@@ -4,10 +4,7 @@ import { addTournament } from 'components/drawers/addTournamentDrawer';
 import { fetchTournament } from 'components/modals/fetchTournament';
 import { getLoginState } from 'services/authentication/loginState';
 import { controlBar } from 'components/controlBar/controlBar';
-import { serverSync } from 'services/storage/serverSync';
-// import { mainMenu } from 'components/menus/mainMenu';
 import { mockTournaments } from './mockTournaments';
-// import { context } from 'services/context';
 
 import { LEFT, RIGHT, SUPER_ADMIN, TOURNAMENTS_CONTROL } from 'constants/tmxConstants';
 
@@ -20,7 +17,6 @@ export function calendarControls(table) {
     { label: 'Import tournament', onClick: () => importTournaments(table) },
     admin && { label: 'Load by ID', onClick: () => fetchTournament({ table }) },
     { divider: true },
-    { label: 'Sync with Server', onClick: serverSync, close: true },
     { divider: true },
     { label: 'Example Tournaments', onClick: () => mockTournaments(table), close: true },
   ].filter(Boolean);
@@ -29,19 +25,6 @@ export function calendarControls(table) {
 
   const items = [
     { label: 'Actions', options: actions, align: RIGHT },
-    /*
-    {
-      label: 'Menu',
-      onClick: () =>
-        context.drawer.open({
-          title: state?.provider?.providerName || 'TMX',
-          context: TMX_TOURNAMENTS,
-          content: mainMenu,
-          width: '200px',
-          side: RIGHT,
-        }),
-    },
-    */
     {
       onKeyDown: (e) => e.keyCode === 8 && e.target.value.length === 1 && setSearchFilter(''),
       onChange: (e) => setSearchFilter(e.target.value),
