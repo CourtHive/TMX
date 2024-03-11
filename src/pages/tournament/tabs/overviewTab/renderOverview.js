f8145518cda27e916f546c04f2e4e83049dc429d
TimeTennis <admin@time.tennis>
2024-03-11 14:39:54 -0400

    feat: :construction_worker: begin work on overview editor

--- src/pages/tournament/tabs/overviewTab/renderOverview.js
+++ src/pages/tournament/tabs/overviewTab/renderOverview.js
@@ -1,5 +1,6 @@
 import { removeAllChildNodes } from 'services/dom/transformers';
 import { tournamentEngine } from 'tods-competition-factory';
+import { overviewControl } from './overviewControl';
 
 import { TOURNAMENT_OVERVIEW } from 'constants/tmxConstants';
 
@@ -7,16 +8,22 @@ export function renderOverview() {
   const element = document.getElementById(TOURNAMENT_OVERVIEW);
   element.style.minHeight = `${window.innerHeight * 0.9}px`;
   removeAllChildNodes(element);
+
   const header = document.createElement('div');
   header.style.fontWeight = 'bold';
   header.className = 'block';
   header.innerHTML = 'Tournament Overview';
   element.appendChild(header);
 
+  const controlAnchor = document.createElement('div');
+  element.appendChild(controlAnchor);
+
   const notes = tournamentEngine.getTournament().tournamentRecord?.notes;
-  if (notes) {
-    const notesView = document.createElement('div');
-    notesView.innerHTML = notes;
-    element.appendChild(notesView);
-  }
+  const notesView = document.createElement('div');
+  notesView.className = 'ql-container ql-snow';
+  notesView.style.border = 'none';
+  notesView.innerHTML = notes ?? '';
+  notesView.id = 'notes';
+  element.appendChild(notesView);
+  overviewControl({ controlAnchor });
 }
