7ba4dcabffb1662e54be3a2e2edc126da469507f
TimeTennis <admin@time.tennis>
2024-03-19 08:30:53 -0400

    refactor: :technologist: add verbose logging option

--- src/services/storage/saveTournamentRecord.js
+++ src/services/storage/saveTournamentRecord.js
@@ -1,9 +1,10 @@
 import { tournamentEngine } from 'tods-competition-factory';
 import { isFunction } from 'functions/typeOf';
 import { tmx2db } from './tmx2db';
+import { env } from 'settings/env';
 
 export async function saveTournamentRecord(params) {
-  console.log('%c localSave', 'color: yellow');
+  env.log?.verbose && console.log('%c localSave', 'color: yellow');
   const tournamentRecord = params?.tournamentRecord ?? tournamentEngine.getTournament()?.tournamentRecord;
   await tmx2db.addTournament(tournamentRecord);
   isFunction(params?.callback) && params.callback();
