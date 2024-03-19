59210698f23f3d2092332b8836e76a71600d07a6
TimeTennis <admin@time.tennis>
2024-03-19 10:27:31 -0400

    feat: :sparkles: add provider online resource

--- src/components/modals/createProvider.ts
+++ src/components/modals/createProvider.ts
@@ -1,15 +1,33 @@
 import { nameValidator } from 'components/validators/nameValidator';
 import { renderForm } from 'components/renderers/renderForm';
+import { addProvider } from 'services/apis/servicesApi';
 import { openModal } from './baseModal/baseModal';
 import { tools } from 'tods-competition-factory';
 import { isFunction } from 'functions/typeOf';
-import { addProvider } from 'services/apis/servicesApi';
+
+// constants
+import { NONE } from 'constants/tmxConstants';
 
 export function createProviderModal(callback) {
-  let inputs;
+  let inputs, imageLoaded;
+
+  const clearImage = () => {
+    const imageEl = document.getElementById('tournamentImagePreview');
+    imageEl.style.display = NONE;
+  };
+
+  const attemptLoad = () => {
+    enableSubmit({ inputs });
+    imageLoaded = false;
+    const imageEl = document.getElementById('tournamentImagePreview');
+    imageEl.onload = () => (imageLoaded = true) && enableSubmit({ inputs });
+    imageEl['src'] = inputs.tournamentImage.value;
+    imageEl.style.display = '';
+  };
 
   const enableSubmit = ({ inputs }) => {
     const isValid =
+      (imageLoaded || !inputs.tournamentImage.value || imageLoaded === undefined) &&
       nameValidator(10)(inputs.providerName.value) &&
       nameValidator(3)(inputs.providerAbbr.value) &&
       (!inputs.providerId.value.length || nameValidator(36)(inputs.providerId.value));
@@ -32,7 +50,7 @@ export function createProviderModal(callback) {
     },
   ];
 
-  const content = (elem) =>
+  const form = (elem) =>
     (inputs = renderForm(
       elem,
       [
@@ -56,16 +74,43 @@ export function createProviderModal(callback) {
           autocomplete: 'off',
           field: 'providerId',
         },
+        {
+          onKeyUp: (val) => val.key === 'Enter' && attemptLoad(),
+          onChange: () => {
+            return inputs.tournamentImage.value ? attemptLoad() : clearImage();
+          },
+          label: 'Web address of online image',
+          placeholder: 'Image URL',
+          field: 'tournamentImage',
+          autocomplete: 'off',
+        },
       ],
       relationships,
     ));
 
+  const content = document.createElement('div');
+  form(content);
+  const image = document.createElement('img');
+  image.id = 'tournamentImagePreview';
+
+  image.style.width = '100%';
+  content.appendChild(image);
   const submitProvider = () => {
     const organisationAbbreviation = inputs.providerAbbr.value;
     const organisationName = inputs.providerName.value;
     const organisationId = tools.UUID();
 
-    const provider = { organisationAbbreviation, organisationName, organisationId };
+    const provider: any = { organisationAbbreviation, organisationName, organisationId };
+    if (imageLoaded) {
+      const url = inputs.tournamentImage.value;
+      const onlineResource = {
+        resourceSubType: 'IMAGE',
+        name: 'tournamentImage',
+        resourceType: 'URL',
+        identifier: url,
+      };
+      provider.onlineResources = [onlineResource];
+    }
 
     const response = (res) => {
       console.log(res);
@@ -80,11 +125,11 @@ export function createProviderModal(callback) {
     buttons: [
       { label: 'Cancel', intent: 'none', close: true },
       {
-        label: 'Invite',
+        onClick: submitProvider,
         intent: 'is-primary',
         id: 'createButton',
+        label: 'Create',
         disabled: true,
-        onClick: submitProvider,
         close: true,
       },
     ],
