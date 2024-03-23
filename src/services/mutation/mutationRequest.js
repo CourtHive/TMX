24692b73de64e97d6d0892498a254bf477b212bb
CourtHive <charles@courthive.com>
2024-03-23 07:46:29 -0400

    feat: :children_crossing: re-style login state on user token expiration

--- src/services/mutation/mutationRequest.js
+++ src/services/mutation/mutationRequest.js
@@ -1,5 +1,5 @@
+import { getLoginState, styleLogin } from 'services/authentication/loginState';
 import { saveTournamentRecord } from 'services/storage/saveTournamentRecord';
-import { getLoginState } from 'services/authentication/loginState';
 import { tmxToast } from 'services/notifications/tmxToast';
 import { emitTmx } from 'services/messaging/socketIo';
 import * as factory from 'tods-competition-factory';
@@ -68,7 +68,11 @@ function queryDateRange({ providerIds, mutate }) {
 function checkPermissions({ providerIds, mutate }) {
   const state = getLoginState();
   // if the tournamentRecord(s) are associated with a providerId, check permissions
-  if (!state) return tmxToast({ message: 'Not logged in', intent: 'is-warning' });
+  if (!state) {
+    context.provider = undefined;
+    styleLogin(false);
+    return tmxToast({ message: 'Not logged in', intent: 'is-warning' });
+  }
 
   const isProvider = !!(
     state?.providerIds?.includes(providerIds[0]) || state?.provider?.organisationId === providerIds[0]
