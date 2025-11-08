66f8c7b5cd43ce8a9028d87ca48927c872c76529
CourtHive <courthive@gmail.com>
2025-11-08 17:02:59 -0500

    refactor: :recycle: typescript conversion - config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,7 @@
+/**
+ * Application configuration and initialization.
+ * Sets up database and event listeners.
+ */
 import { processDirective } from 'services/processDirective';
 import { tmx2db } from 'services/storage/tmx2db';
 import { context } from 'services/context';
@@ -6,7 +10,7 @@ import { env } from 'settings/env';
 
 import { PROCESS_DIRECTIVE } from 'constants/comsConstants';
 
-export function initConfig() {
+export function initConfig(): Promise<void> {
   return new Promise((resolve, reject) => {
     initListeners();
 
@@ -22,47 +26,33 @@ export function initConfig() {
 }
 
 const catchAsync =
-  (fn) =>
-  (...args) =>
+  (fn: (...args: any[]) => Promise<any>) =>
+  (...args: any[]) =>
     isDev() ? fn(...args) : fn(...args).catch((err) => console.log(err));
 
-function initDB() {
+function initDB(): Promise<void> {
   return new Promise((resolve, reject) => {
-    const initTMXdb = () => new Promise((rz, rj) => catchAsync(tmx2db.initDB)().then(resolve, dbUpgrade).then(rz, rj));
+    const initTMXdb = () => new Promise<void>((rz, rj) => catchAsync(tmx2db.initDB)().then(() => resolve(), dbUpgrade).then(rz, rj));
     initTMXdb().then(resolve, reject);
   });
 }
 
-// older versions of Dexie required this step
-function dbUpgrade() {
+function dbUpgrade(): void {
   const message = '<h2>Database Upgraded</h2><div style="margin: 1em;">Please Close Browser and Re-launch</div>';
   context.modal.inform({ message });
 }
 
-function addMessage(msg) {
-  const msgHash = (m) =>
+function addMessage(msg: any): void {
+  const msgHash = (m: any) =>
     Object.keys(m)
       .map((key) => m[key])
       .join('');
   const messageHash = msgHash(msg);
-  const exists = env.messages.reduce((p, c) => (msgHash(c) === messageHash ? true : p), false);
+  const exists = env.messages.reduce((p: boolean, c: any) => (msgHash(c) === messageHash ? true : p), false);
   if (!exists) env.messages.push(msg);
 }
 
-/*
-function tmxMessage(msg) {
-  console.log({ tmxMessage: msg });
-  if (msg.authorized && (msg.tuid || msg.tournamentId)) {
-    authMessage(msg);
-  } else {
-    msg.notice = msg.notice || msg.tournament;
-    if (msg.notice) addMessage(msg);
-  }
-}
-*/
-
-function initListeners() {
+function initListeners(): void {
   context.ee.addListener(PROCESS_DIRECTIVE, processDirective);
-  // context.ee.addListener('tmxMessage', tmxMessage);
   context.ee.addListener('addMessage', addMessage);
 }
