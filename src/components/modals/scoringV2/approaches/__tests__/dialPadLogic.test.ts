5fa77babfb05f79f224750af49828ea58eb7b314
CourtHive <courthive@gmail.com>
2026-01-01 16:57:15 -0500

    refactor: use MATCH_FORMATS constants in tests
    - Import MATCH_FORMATS from constants file
    - Replace all hardcoded format strings with constants
    - Makes tests more maintainable and consistent
    - All 9 tests still passing

--- src/components/modals/scoringV2/approaches/__tests__/dialPadLogic.test.ts
+++ src/components/modals/scoringV2/approaches/__tests__/dialPadLogic.test.ts
@@ -5,6 +5,7 @@
 
 import { describe, it, expect } from 'vitest';
 import { formatScoreString } from '../dialPadLogic';
+import { MATCH_FORMATS } from '../../../../../constants/matchUpFormats';
 
 type TestCase = {
   name: string;
@@ -19,25 +20,25 @@ const testCases: TestCase[] = [
     name: 'should handle 6-4 6-4',
     keySequence: [6, 4, 6, 4],
     expectedScoreString: '6-4 6-4',
-    matchUpFormat: 'SET3-S:6/TB7',
+    matchUpFormat: MATCH_FORMATS.SET3_S6_TB7,
   },
   {
     name: 'should handle 3-6 3-6',
     keySequence: [3, 6, 3, 6],
     expectedScoreString: '3-6 3-6',
-    matchUpFormat: 'SET3-S:6/TB7',
+    matchUpFormat: MATCH_FORMATS.SET3_S6_TB7,
   },
   {
     name: 'should handle 7-5 6-3',
     keySequence: [7, 5, 6, 3],
     expectedScoreString: '7-5 6-3',
-    matchUpFormat: 'SET3-S:6/TB7',
+    matchUpFormat: MATCH_FORMATS.SET3_S6_TB7,
   },
   {
     name: 'should handle 6-0 6-1',
     keySequence: [6, 0, 6, 1],
     expectedScoreString: '6-0 6-1',
-    matchUpFormat: 'SET3-S:6/TB7',
+    matchUpFormat: MATCH_FORMATS.SET3_S6_TB7,
   },
   
   // Tiebreak sets (two-digit tiebreak scores)
@@ -45,19 +46,19 @@ const testCases: TestCase[] = [
     name: 'should handle 6-7 with tiebreak 03-06',
     keySequence: [6, 7, 0, 3, 0, 6],
     expectedScoreString: '6-7(03-06)',
-    matchUpFormat: 'SET3-S:6/TB7',
+    matchUpFormat: MATCH_FORMATS.SET3_S6_TB7,
   },
   {
     name: 'should handle 7-6 with tiebreak 05-03',
     keySequence: [7, 6, 0, 5, 0, 3],
     expectedScoreString: '7-6(05-03)',
-    matchUpFormat: 'SET3-S:6/TB7',
+    matchUpFormat: MATCH_FORMATS.SET3_S6_TB7,
   },
   {
     name: 'should handle 6-6 tiebreak 07-05',
     keySequence: [6, 6, 0, 7, 0, 5],
     expectedScoreString: '6-6(07-05)',
-    matchUpFormat: 'SET3-S:6/TB7',
+    matchUpFormat: MATCH_FORMATS.SET3_S6_TB7,
   },
   
   // Single tiebreak set
@@ -65,7 +66,7 @@ const testCases: TestCase[] = [
     name: 'should handle TB10 format 12-10',
     keySequence: [1, 2, 1, 0],
     expectedScoreString: '12-10',
-    matchUpFormat: 'SET1-S:T10',
+    matchUpFormat: MATCH_FORMATS.SET1_S_TB10,
   },
   
   // SET5 format
@@ -73,7 +74,7 @@ const testCases: TestCase[] = [
     name: 'should handle SET5 6-4 6-4',
     keySequence: [6, 4, 6, 4],
     expectedScoreString: '6-4 6-4',
-    matchUpFormat: 'SET5-S:6/TB7',
+    matchUpFormat: MATCH_FORMATS.SET5_S6_TB7,
   },
 ];
 
