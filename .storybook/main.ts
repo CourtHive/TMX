d255efd055e9d05f251a983017b24e02b0e92cb2
CourtHive <courthive@gmail.com>
2026-01-05 10:32:58 -0500

    build: :package: upgrade storybook to v10

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,8 +4,7 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    '@storybook/addon-docs',
   ],
   framework: {
     name: '@storybook/html-vite',
